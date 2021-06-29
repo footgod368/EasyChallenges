d2a050653e091102f1f5d7513d53df04a2ee5cb4
root <root@PCMICRO-2LJUBAK.localdomain>
2021-06-29 20:26:58 +0800

    Brick Constants and final definition

--- src/Modules/Brick.elm
+++ src/Modules/Brick.elm
@@ -49,3 +49,58 @@ type BrickCollision
     | CollideAfterEvent Int BrickCollision
     | NoCollideAfterEvent Int BrickCollision
     | NoNextBrickCollision
+
+
+
+{-| `BrickMove` describes whether and how the brick will move in the game. `Move (arrayPos : Array Pos) (speed :
+Float) (nextMoveEventID : Int) (nextBrickMove : BrickMove)`: arrayPos means the way the block moves, speed is how fast
+the block moves each frame, nextMoveEventID is when this stage of movement is finished, what movement happens next,
+nextBrickMove is next move, with None indicating no further movements take place. Note, if the first stage is no
+movement, then it will move when `Event` whose id = 1 move, write like this, a null array will not make the block move:
+
+    blockMove : BrickMove
+    blockMove =
+        Move (Array.fromList [])
+            0.0
+            1
+            (Move (Array.fromList [ ( 0.0, 100.0 ), ( 100.0, 100.0 ) ]) 10.0 -1 None)
+
+-}
+type BrickMove
+    = Move (Array GlobalBasics.Pos) Float Int BrickMove
+    | NoNextBrickMove
+
+
+{-| For future different shapes of blocks.
+-}
+type BrickAppearance
+    = NoAppearance
+    | Grass
+
+
+{-| brickWidth Constant
+-}
+brickWidth : Float
+brickWidth =
+    --30.0
+    Tuple.first GlobalBasics.blockSize
+
+
+{-| brickHeight Constant
+-}
+brickHeight : Float
+brickHeight =
+    Tuple.second GlobalBasics.blockSize
+
+
+{-| `Brick` is a record of the block unit. See detail definitions in individual definition.
+-}
+type alias Brick =
+    { pos : GlobalBasics.Pos
+    , collisionBox : GlobalBasics.CollisionBox
+    , appearance : BrickAppearance
+    , brickVisibility : BrickVisibility
+    , brickCollision : BrickCollision
+    , brickMove : BrickMove
+    }
+
