c4cdf0f95b6d90b34b640ce4ed4a5b935c85b438
weiyichen2012 <ethepherein@sjtu.edu.cn>
2021-07-12 21:39:49 +0800

    add move to NoticeBoard to make sure level2 can be completed as planned

--- src/Modules/NoticeBoard.elm
+++ src/Modules/NoticeBoard.elm
@@ -6,9 +6,10 @@ module NoticeBoard exposing (NoticeBoard)
 
 -}
 
+import Array exposing (Array)
 import GlobalBasics
 
-{-| Same as Brick's visibility, except adding new words that can be changed. The words are stored in
+{-| Almost same as Brick's visibility, except adding new words that can be changed. The words are stored in
 NoticeBoardVisibility. You can use it like this:
 
     newNoticeBoardVisibility : NoticeBoardVisibility
@@ -27,12 +28,39 @@ type NoticeBoardVisibility
     | Invisible NoticeBoardVisibility
     | VisibleAfterEvent Int String NoticeBoardVisibility
     | InvisibleAfterEvent Int NoticeBoardVisibility
-    | NoNextBrickVisibility
+    | NoNextNoticeBoardVisibility
 
+{-| Same as Brick move.
+-}
+type NoticeBoardMove
+    = Move (Array GlobalBasics.Pos) Float Int NoticeBoardMove
+    | NoNextNoticeBoardMove
 
 {-| Definition of noticeBoard, it's information is in String.
 -}
 type alias NoticeBoard =
     { pos : GlobalBasics.Pos
     , visibility : NoticeBoardVisibility
+    , move : NoticeBoardMove
+    , fontSize : Int
+    }
+
+{-| Full init of the NoticeBoard
+-}
+init : GlobalBasics.Pos -> NoticeBoardVisibility -> NoticeBoardMove -> Int -> NoticeBoard
+init pos noticeBoardVisibility noticeBoardMove fontSize =
+    { pos = pos
+    , visibility = noticeBoardVisibility
+    , move = noticeBoardMove
+    , fontSize = fontSize
+    }
+
+{-| Quick init, always visible.
+-}
+quickInit : GlobalBasics.Pos -> String -> Int -> NoticeBoard
+quickInit pos info fontSize =
+    { pos = pos
+    , visibility = Visible info NoNextNoticeBoardVisibility
+    , move = NoNextNoticeBoardMove
+    , fontSize = fontSize
     }
