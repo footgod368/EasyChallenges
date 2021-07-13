a1ba6a5bd9803aef944acc38a8fa8f8d4a5a933b
Universe.chen <universe.chen@sjtu.edu.cn>
2021-07-13 18:16:28 +0800

    monster

--- src/Modules/Monster.elm
+++ src/Modules/Monster.elm
@@ -16,8 +16,8 @@ type alias Monster =
     , appearance : MonsterAppearance
     , xMode: MonsterXMode
     , yMode: MonsterYMode
-    , mode: MonsterMode
-    , speed: Float
+    , xSpeed: Float
+    , ySpeed: Float
     }
 
 type MonsterAppearance 
@@ -25,25 +25,24 @@ type MonsterAppearance
 
 type MonsterXMode
     = Stop
+    | Listen Float
     | Move
 
 type MonsterYMode 
     = Stop
+    | Listen Float
     | Follow
 
-type MonsterMode
-    = ChangeMode MonsterXMode MonsterYMode Float Int MonsterMode
-    | NoNextChangeMode
 
-init : ( Float, Float ) -> MonsterAppearance -> MonsterXMode -> MonsterYMode -> MonsterMode -> Float -> Monster
-init ( x, y ) monsterAppearance monsterX monsterY monsterMode speed =
+init : ( Float, Float ) -> MonsterAppearance -> MonsterXMode -> MonsterYMode ->  Float -> Monster
+init ( x, y ) monsterAppearance monsterX monsterY xSpeed =
     { pos = ( x, y )
     , collisionBox = monsterCollisionBox monsterAppearance
     , appearance = monsterAppearance
     , xMode = monsterX
     , yMode = monsterY
-    , mode = monsterMode
-    , speed = speed
+    , xSpeed = xSpeed
+    , ySpeed = 0
     }
 
 {-| default collisionBox
@@ -60,4 +59,58 @@ monsterCollisionBox monsterAppearance =
                     , ( ( 0.0, height ), ( 0.0, 0.0 ) )
                     ]
                 )
+
+defMonster : Monster
+defMonster = init ( 0, 0 ) ( MonsterA 20 20 ) Stop Stop 1
+
+update : ( { model | player : Player.Player, monsters : Array Monster, actEvent : Array Event.ActEvent }, Cmd MainType.Msg ) -> ( { model | player : Player.Player, monsters : Array Monster, actEvent : Array Event.ActEvent }, Cmd MainType.Msg )
+update ( model, cmd ) =
+    ( List.foldl updateOneMonster model (List.range 0 (Array.length model.monsters - 1)), cmd )
+
+
+{-| update one monster. Used in update. Not exposed.
+-}
+updateOneMonster : Int -> { model | player : Player.Player, monsters : Array Monster, actEvent : Array Event.ActEvent } -> { model | player : Player.Player, monsters : Array Monster, actEvent : Array Event.ActEvent }
+updateOneMonster id model =
+    model
+        |> updateOneMonsterMode id
+        |> updateOneMonsterMove id
+        |> updateOneMonsterCollision id
+
+updateOneMonsterMode : Int -> { model | player : Player.Player, monsters : Array Monster, actEvent : Array Event.ActEvent } -> { model | player : Player.Player, monsters : Array Monster, actEvent : Array Event.ActEvent }
+updateOneMonsterMode id model =
+    let
+        monster =
+            Array.get id model.monsters
+                |> withDefault defMonster
+        
+        dx = (Tuple.first model.player.pos) - (Tuple.first monster.pos)
+
+        newXMode =
+            case monster.xMode of 
+                Listen x ->
+                    if dx < x && dx > (-x) then
+                        Move
+                    else
+                        Listen
+                Stop -> Stop
+                Move -> Move
+        
+        newYMode =
+            case monster.yMode of 
+                Listen x ->
+                    if dx < x && dx > (-x) then
+                        Follow
+                    else
+                        Listen
+                Stop -> Stop
+                Follow -> Follow
+        
+        newMonster = { monster | xMode = newXMode, yMode = newYMode }
+
+        newMonsters = Array.set id newMonster model.monsters
+
+        newModel = { model | monsters = newMonsters }
+    in
+    newModel
 
