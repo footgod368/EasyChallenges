7cf1e5eed722f18875201c6152f7d662f1c41edd
weiyichen2012 <ethepherein@sjtu.edu.cn>
2021-07-18 15:09:05 +0800

    remove useless files

--- elm-doc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-elm-doc . --fake-license "MIT" -o ".elm-doc" --fake-user "SilverFOCS-21" --fake-project "pxteam7" --mount-at "/test"
--- replace.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i "s/import $1/import Menu.$1 as $1/g" `grep . -rl ./src`
