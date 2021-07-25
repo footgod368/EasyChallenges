90303cfa638ea3db300ceee03938d9ab5040becd
weiyichen2012 <ethepherein@sjtu.edu.cn>
2021-07-25 23:19:36 +0800

    add sound constants

---
+++ src/Modules/Sound.elm
@@ -0,0 +1,37 @@
+module Modules.Sound exposing (..)
+
+import Array exposing (Array)
+import Html exposing (Html)
+import Html.Attributes as HtmlAttr
+import MainFunction.MainType as MainType
+import Svg exposing (Svg)
+
+
+backGroundLength : Int
+backGroundLength =
+    10
+
+
+jumpLength : Int
+jumpLength =
+    30
+
+
+randomBoxLength : Int
+randomBoxLength =
+    60
+
+
+needleLength : Int
+needleLength =
+    30
+
+
+deadLength : Int
+deadLength =
+    200
+
+
+swordLength : Int
+swordLength =
+    200
