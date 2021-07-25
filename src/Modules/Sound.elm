685f78f4c18ea097118cf58f2e136961f0efa5dc
weiyichen2012 <ethepherein@sjtu.edu.cn>
2021-07-25 23:20:28 +0800

    add viewOneSoundTrigger

--- src/Modules/Sound.elm
+++ src/Modules/Sound.elm
@@ -62,3 +62,62 @@ type alias Sound =
     { soundTrigger : List SoundTrigger
     }
 
+
+{-| Input the list of soundTrigger to init
+-}
+init : List SoundTrigger -> Sound
+init soundTriggerList =
+    Sound soundTriggerList
+
+
+viewOneSoundTrigger : SoundTrigger -> List (Svg MainType.Msg)
+viewOneSoundTrigger soundTrigger =
+    case soundTrigger of
+        Event eventId soundEffect ->
+            []
+
+        Activated n soundEffect nextSoundTrigger ->
+            [ Html.audio
+                (List.append
+                    [ HtmlAttr.controls True
+                    , HtmlAttr.autoplay True
+                    , HtmlAttr.loop False
+                    ]
+                    (case soundEffect of
+                        BackGround ->
+                            [ HtmlAttr.src "assets/backgroundMusic.ogg"
+                            , HtmlAttr.id "BackGround"
+                            ]
+
+                        Jump ->
+                            [ HtmlAttr.src "assets/jumpMusic.ogg"
+                            , HtmlAttr.id "Jump"
+                            ]
+
+                        RandomBox ->
+                            [ HtmlAttr.src "assets/randomBoxMusic.ogg"
+                            , HtmlAttr.id "RandomBox"
+                            ]
+
+                        Needle ->
+                            [ HtmlAttr.src "assets/needleMusic.ogg"
+                            , HtmlAttr.id "Needle"
+                            ]
+
+                        Sword ->
+                            [ HtmlAttr.src "assets/swordMusic.ogg"
+                            , HtmlAttr.id "Sword"
+                            ]
+
+                        Dead ->
+                            [ HtmlAttr.src "assets/deadMusic.ogg"
+                            , HtmlAttr.id "Dead"
+                            ]
+                    )
+                )
+                []
+            ]
+
+        None ->
+            []
+
