8d8f994bab66a4573e1e454e81f592e14bba390d
johnrhimawan <johnr.himawan@gmail.com>
2021-07-30 13:50:08 +0700

    update

--- src/Level6/Level6Type.elm
+++ src/Level6/Level6Type.elm
@@ -24,7 +24,7 @@ import Modules.SavePoint as SavePoint
 import Modules.Sound as Sound
 
 
-{-| `Model` that used in level6
+{-| `Model` that used in level6. See in level1Type, highly repetition.
 -}
 type alias Model =
     { windowBoundary : GlobalBasics.Pos
